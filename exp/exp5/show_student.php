2a505a9664c2041621e944f28cdacd325d77a8dc
Beth-Chan <xfchen1109@foxmail.com>
2018-05-14 10:37:34 +0800

    数据库取数据命名规范

---
+++ exp/exp5/show_student.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>题目列表</title>
+    <style> 
+    table {
+        width: 900px;
+        margin: 30px auto;
+        border-collapse: collapse;
+        text-align: center;
+    }
+    tr, td {
+        width: 270px;
+        height: 50px;
+    }
+    form {
+        text-align: center;
+    }
+    table tr:first-child {
+        background: yellow;
+    }
+    tr:nth-child(even) {
+        background: lightgreen;
+    }
+    </style>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>序号</td>
+            <td>学号</td>
+            <td>姓名</td>
+            <td>题目</td>
+            <td>状态</td>
+            <td>录入时间</td>
+            <td>合作学生</td>
+        </tr>
+    <div class="paganation">
+        <?php
+        require_once('MyDB.php');
+        $db = new MyDB();
+        $records = 3; // 每页显示的记录数
+        $page = 1; // 表示显示的页码
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+        }
+        $numStart = ($page - 1) * $records; // 计算起始记录, 0 开始
+        $selectSQL = "SELECT * FROM students LIMIT $numStart, $records";
+        $result = $db -> execSQL($selectSQL);
+
+        $i = 0;
+        while ($obj = $result -> fetch_assoc()) {
+            ++$i;
+            echo "<tr><td>{$i}</td>";
+            echo "<td>{$obj['sno']}</td>";
+            echo "<td>{$obj['name']}</td>";
+            echo "<td>{$obj['title']}</td>";
+            echo "<td>{$obj['state']}</td>";
+            echo "<td>{$obj['last_time']}</td>";
+            echo "<td>{$obj['partner']}</td>";
+            echo '</tr>';
+        }
+        ?>
+    </table>
+    
+    <form action="show_student.php" method="GET">
+        <input type="submit" value="翻到"/>
+        <?php
+            echo "<input type=\"text\" name=\"page\" size=\"2\" value=\"{$page}\"/>";
+            echo '<a href="show_student.php?page=', $page-1, '">前一页</a>';
+            echo '<a href="show_student.php?page=', $page+1, '">下一页</a>';
+        ?>
+    </form>
+    </div>
+        
+    
+</body>
+</html>
